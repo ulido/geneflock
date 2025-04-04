895ebebdce9fb6b1c47436e37e8b979b093bdc96
Ulrich Dobramysl <ulido@users.noreply.github.com>
2025-04-04 13:44:17 +0100

    Rudimentary docs, PEP8 linting

--- src/geneflock/geneflock.py
+++ src/geneflock/geneflock.py
@@ -1,20 +1,31 @@
+"""geneflock.py
+
+Module to render a genome to SVG, each chromosome represented as a line, with
+colored boxes representing genes.
+"""
+
 import pathlib
 from geffa.geffa import GffFile, SequenceRegion, GeneNode
 
+
 class Gene:
+    """Class rendering a single gene."""
     def __init__(self, genenode: GeneNode):
         self.genenode = genenode
 
     @property
     def extent(self):
+        """Length of the gene in basepairs."""
         return self.genenode.end - self.genenode.start + 1
-    
+
     @property
     def id(self):
+        """Unique gene ID, formatted to act as as CSS selector."""
         return self.genenode.attributes["ID"].replace(".", "_")
 
     @property
     def direction(self):
+        """Gene direction (left-right or right-left)"""
         if self.genenode.strand == '+':
             return "left-right"
         elif self.genenode.strand == '-':
@@ -22,6 +33,7 @@ class Gene:
         raise ValueError("GeneNode has no strand!")
 
     def render(self):
+        """Render the gene as SVG element(s)."""
         d = -1 if self.genenode.strand == "+" else 1
         f = (1-d)*0.5
         return f'''
@@ -34,19 +46,24 @@ class Gene:
             class="gene {self.direction}"
             id="{self.id}"/>'''
 
+
 class Chromosome:
+    """Class rendering a chromosome."""
     def __init__(self, seqreg: SequenceRegion):
         self.seqreg = seqreg
 
     @property
     def id(self):
+        """Unique chromosome ID, formatted to act as as CSS selector."""
         return self.seqreg.name.replace(".", "_")
 
     @property
     def extent(self):
+        """Length of the chromosome in basepairs."""
         return self.seqreg.end - self.seqreg.start + 1
-    
+
     def render(self, offset=0):
+        """Render the chromosome and any genes contained within as SVG elements."""
         s = f'<g transform="translate(0 {offset})" id="{self.id}">'
         s += f'<text y="0.2" transform="scale(10000, 1)" class="chromosome-name">{self.seqreg.name}</text>'
         for gene in (feature for feature in self.seqreg.node_registry.values() if feature.type == "gene"):
@@ -54,55 +71,72 @@ class Chromosome:
         s += f'<path d="M 0,0.5 h {self.extent}" class="chromosome"/></g>'
         return s
 
+
+DEFAULT_STYLE = """
+.background {
+    fill: white;
+}
+
+.chromosome {
+    stroke: black;
+    stroke-width: 0.1px;
+    zorder: 100;
+}
+
+.gene {
+    height: 0.25px;
+}
+
+.left-right {
+    fill: red;
+}
+
+.right-left {
+    fill: blue;
+}
+
+.chromosome-name {
+    font-size: 0.25px;
+    font-family: sans-serif;
+}
+"""
+
+
 class Genome:
+    """Class rendering a genome (given as a GFF file) as an SVG drawing."""
     def __init__(self, gfffile: str | pathlib.Path | GffFile, filter_chromosomes=lambda c: c.extent > 100000):
         if not isinstance(gfffile, GffFile):
             gfffile = GffFile(gfffile)
         self.gfffile = gfffile
 
-        self.chromosomes = [Chromosome(seqreg) for seqreg in self.gfffile.sequence_regions.values() if len(seqreg.node_registry) > 0]
-        self.chromosomes = [chromosome for chromosome in self.chromosomes if filter_chromosomes(chromosome)]
+        self.chromosomes = [Chromosome(seqreg) for seqreg in self.gfffile.sequence_regions.values(
+        ) if len(seqreg.node_registry) > 0]
+        self.chromosomes = [
+            chromosome for chromosome in self.chromosomes if filter_chromosomes(chromosome)]
 
     @property
     def height(self):
+        """The 'height' of the genome (the number of chromosomes contained)."""
         return len(self.chromosomes)
-    
+
     @property
     def width(self):
+        """The 'width' of the genome (the length in basepairs of the longest chromosome)."""
         return max([chromosome.extent for chromosome in self.chromosomes])
-    
+
     def style(self):
-        return """
-            .background {
-                fill: white;
-            }
+        """CSS styles of the rendered SVG file."""
+        return DEFAULT_STYLE
 
-            .chromosome {
-                stroke: black;
-                stroke-width: 0.1px;
-                zorder: 100;
-            }
-
-            .gene {
-                height: 0.25px;
-            }
-
-            .left-right {
-                fill: red;
-            }
-
-            .right-left {
-                fill: blue;
-            }
-
-            .chromosome-name {
-                font-size: 0.25px;
-                font-family: sans-serif;
-            }
-"""
-    
     def render(self):
-        s = f'<svg viewBox="0 0 {self.width} {self.height}" preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg" height="{self.height * 100}" width="{self.width / 100}">'
+        """Render the genome with all chromosomes contained therein as a SVG file."""
+        s = f'''<svg
+            viewBox="0 0 {self.width} {self.height}"
+            preserveAspectRatio="none"
+            xmlns="http://www.w3.org/2000/svg"
+            height="{self.height * 100}"
+            width="{self.width / 100}">
+        '''
         s += f"<style>{self.style()}</style>"
         s += '<rect height="100%" width="100%" class="background"/>'
         for offset, chromosome in enumerate(self.chromosomes):
--- src/geneflock/genehighlight.py
+++ src/geneflock/genehighlight.py
@@ -1,7 +1,14 @@
+"""genehightlight.py
+
+Extends geneflock.py to allow highlighting of individual genes.
+"""
+
 from .geneflock import Genome
 
+
 class HighlightIDListGenome(Genome):
-    def __init__(self, idlist: list[str], *args, highlightcolor: str="green", **kwargs):
+    """Subclass of Genome, allowing one to specify a list of highlighted genes."""
+    def __init__(self, idlist: list[str], *args, highlightcolor: str = "green", **kwargs):
         self.idlist = idlist
         self.highlightcolor = highlightcolor
         super().__init__(*args, **kwargs)
@@ -9,6 +16,7 @@ class HighlightIDListGenome(Genome):
     def style(self):
         styles = super().style()
 
-        highlightlist = ','.join(["#" + entry.replace(".", "_") for entry in self.idlist])
+        highlightlist = ','.join(["#" + entry.replace(".", "_")
+                                 for entry in self.idlist])
         styles += highlightlist + f"{{ fill: {self.highlightcolor}; }}"
         return styles
